903c6cd955ea6663346efe4026ae4a56020dbe11
Kazzbo <cedricaadam@gmail.com>
2019-01-16 17:21:17 +0100

    Ebauche

---
+++ Dog.py
@@ -0,0 +1,4 @@
+import arcade
+
+class Dog(arcade.sprite):
+    sprite = arcade.sprite("Image/Sprite/Dog/chien1.png")
--- main.py
+++ main.py
@@ -6,142 +6,80 @@ python -m arcade.examples.shape_list_skylines
 """
 import random
 import arcade
-import time
+import os
 
-SCREEN_WIDTH = 1200
-SCREEN_HEIGHT = 600
+SPRITE_SCALING = 3
 
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
 
-def make_star_field(star_count):
-    """ Make a bunch of circles for stars. """
+MOVEMENT_SPEED = 5
 
-    shape_list = arcade.ShapeElementList()
+class Dog(arcade.Sprite):
+  #  sprite = arcade.sprite("Image/Sprite/Dog/chien1.png")
 
-    for star_no in range(star_count):
-        x = random.randrange(SCREEN_WIDTH)
-        y = random.randrange(SCREEN_HEIGHT)
-        radius = random.randrange(1, 4)
-        brightness = random.randrange(127, 256)
-        color = (brightness, brightness, brightness)
-        shape = arcade.create_rectangle_filled(x, y, radius, radius, color)
-        shape_list.append(shape)
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
 
-    return shape_list
+        if self.left < 0:
+            self.left = 0
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
 
+        if self.bottom < 0:
+            self.bottom = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
 
-def make_skyline(width, skyline_height, skyline_color,
-                 gap_chance = 0.70, window_chance=0.30, light_on_chance=0.5,
-                 window_color=(255, 255, 200), window_margin=3, window_gap=2,
-                 cap_chance=0.20):
-    """ Make a skyline """
-
-    shape_list = arcade.ShapeElementList()
-
-    # Add the "base" that we build the buildings on
-    shape = arcade.create_rectangle_filled(width / 2, skyline_height / 2, width, skyline_height, skyline_color)
-    shape_list.append(shape)
-
-    building_center_x = 0
-
-    skyline_point_list = []
-    color_list = []
-
-    while building_center_x < width:
-
-        # Is there a gap between the buildings?
-        if random.random() < gap_chance:
-            gap_width = random.randrange(10, 50)
-        else:
-            gap_width = 0
-
-        # Figure out location and size of building
-        building_width = random.randrange(20, 70)
-        building_height = random.randrange(40, 150)
-        building_center_x += gap_width + (building_width / 2)
-        building_center_y = skyline_height + (building_height / 2)
-
-        x1 = building_center_x - building_width / 2
-        x2 = building_center_x + building_width / 2
-        y1 = skyline_height
-        y2 = skyline_height + building_height
-
-        skyline_point_list.append( [x1, y1] )
-
-        skyline_point_list.append( [x1, y2] )
-
-        skyline_point_list.append( [x2, y2] )
-
-        skyline_point_list.append( [x2, y1] )
-
-        for i in range(4):
-            color_list.append([skyline_color[0],  skyline_color[1], skyline_color[2]] )
-
-        if random.random() < cap_chance:
-            x1 = building_center_x - building_width / 2
-            x2 = building_center_x + building_width / 2
-            x3 = building_center_x
-
-            y1 = y2 = building_center_y + building_height / 2
-            y3 = y1 + building_width / 2
-
-            shape = arcade.create_polygon([[x1, y1], [x2, y2], [x3, y3]], skyline_color)
-            shape_list.append(shape)
-
-        # See if we should have some windows
-        if random.random() < window_chance:
-            # Yes windows! How many windows?
-            window_rows = random.randrange(10, 15)
-            window_columns = random.randrange(1, 7)
-
-            # Based on that, how big should they be?
-            window_height = (building_height - window_margin * 2) / window_rows
-            window_width = (building_width - window_margin * 2 - window_gap * (window_columns - 1)) / window_columns
-
-            # Find the bottom left of the building so we can start adding widows
-            building_base_y = building_center_y - building_height / 2
-            building_left_x = building_center_x - building_width / 2
-
-            # Loop through each window
-            for row in range(window_rows):
-                for column in range(window_columns):
-                    if random.random() < light_on_chance:
-                        x1 = building_left_x + column * (window_width + window_gap) + window_margin
-                        x2 = building_left_x + column * (window_width + window_gap) + window_width + window_margin
-                        y1 = building_base_y + row * window_height
-                        y2 = building_base_y + row * window_height + window_height * .8
-
-                        skyline_point_list.append( [x1, y1] )
-                        skyline_point_list.append( [x1, y2] )
-                        skyline_point_list.append( [x2, y2] )
-                        skyline_point_list.append( [x2, y1] )
-
-                        for i in range(4):
-                            color_list.append( (window_color[0], window_color[1], window_color[2]) )
-
-        building_center_x += (building_width / 2)
-
-    shape = arcade.create_rectangles_filled_with_colors(skyline_point_list, color_list)
-    shape_list.append(shape)
-
-    return shape_list
-
-
-class MyGame(arcade.Window):
+class Cyberbot(arcade.Window):
     """ Main application class. """
 
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Skyline Using Buffered Shapes")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Cyberbot")
 
-        self.stars = make_star_field(150)
-        self.skyline1 = make_skyline(SCREEN_WIDTH * 5, 250, (80, 80, 80))
-        self.skyline2 = make_skyline(SCREEN_WIDTH * 5, 150, (50, 50, 50))
+        #la list complete des sprites
+        self.all_sprites_list = None
+        self.player = None
+      #  self.player_list = None
 
-        arcade.set_background_color(arcade.color.BLACK)
+      #  self.player_sprite = None
 
     def setup(self):
         """ Set up the game and initialize the variables. """
+
+        self.all_sprites_list = arcade.SpriteList()
+
+        self.player = arcade.AnimatedWalkingSprite()
+
+        character_scale = 2
+        self.player.stand_right_textures = []
+        self.player.stand_right_textures.append(arcade.load_texture("Image/Sprite/Dog/chien1.png",
+                                                                    scale=character_scale))
+
+        self.player.walk_right_textures = []
+
+        self.player.walk_right_textures.append(arcade.load_texture("Image/Sprite/Dog/chien1.png",
+                                                                   scale=character_scale))
+        self.player.walk_right_textures.append(arcade.load_texture("Image/Sprite/Dog/chien2.png",
+                                                                   scale=character_scale))
+        self.player.walk_right_textures.append(arcade.load_texture("Image/Sprite/Dog/chien3.png",
+                                                                   scale=character_scale))
+        self.player.walk_right_textures.append(arcade.load_texture("Image/Sprite/Dog/chien4.png",
+                                                                   scale=character_scale))
+        #What the fuck is that carp
+        self.player.texture_change_distance = 20
+
+        self.player.center_x = SCREEN_HEIGHT // 2
+        self.player.center_y = SCREEN_WIDTH // 2
+
+        self.player.scale = 2
+
+        self.all_sprites_list.append(self.player)
+
+        self.background = arcade.load_texture("Image/Background/cyberpunk-street-files/PNG/cyberpunk-street.png")
 
     def on_draw(self):
         """
@@ -149,26 +87,41 @@ class MyGame(arcade.Window):
         """
 
         # This command has to happen before we start drawing
-
-        start_time = int(round(time.time() * 1000))
         arcade.start_render()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        self.all_sprites_list.draw()
 
-        self.stars.draw()
-        self.skyline1.draw()
-        self.skyline2.draw()
-        end_time = int(round(time.time() * 1000))
-        total_time = end_time - start_time
 
-        arcade.draw_text(f"Time: {total_time}", 10, 10, arcade.color.WHITE)
+
+      #  arcade.draw_texture_rectangle(SCREEN_WIDTH // 4, SCREEN_HEIGHT // 4,
+      #                                  SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
     def update(self, delta_time):
         """ Movement and game logic """
-        self.skyline1.center_x -= 0.5
-        self.skyline2.center_x -= 1
+        self.all_sprites_list.update()
+        self.all_sprites_list.update_animation()
 
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        if key == arcade.key.Z:
+            self.player.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.Q:
+            self.player.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player.change_x = MOVEMENT_SPEED
 
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.Z or key == arcade.key.S:
+            self.player.change_y = 0
+        elif key == arcade.key.Q or key == arcade.key.D:
+            self.player.change_x = 0
 def main():
-    window = MyGame()
+    window = Cyberbot()
     window.setup()
     arcade.run()
 
