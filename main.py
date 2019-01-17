9ca842303ea61ae73365b50f81f9f5e724884830
Unknown <arnaud.chretien@outlook.com>
2019-01-17 15:52:37 +0100

    le robot

--- main.py
+++ main.py
@@ -45,13 +45,13 @@ class Cyberbot(arcade.Window):
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Cyberbot")
 
-        #song atribute
+        # song atribute
         self.main_sound = arcade.load_sound("Sound/main_song.mp3")
         self.mute = 0
 
         self.bullet_list = None
 
-        #la list complete des sprites
+        # la list complete des sprites
         self.all_sprites_list = None
 
         #initialisation des sprites et outils pour le joueur
@@ -59,16 +59,22 @@ class Cyberbot(arcade.Window):
         self.player_sprite = None
         self.player_laser = None
 
+        self.robot = None
+        self.robot_sprite = None
+        self.robot_laser = None
+
         self.physics_engine = None
         self.view_bottom = 0
         self.view_left = 0
         self.count_y = 0
+        self.bloc_list = None
 
     def setup(self):
         """ Set up the game and initialize the variables. """
 
         self.all_sprites_list = arcade.SpriteList()
         self.player_sprite = arcade.SpriteList()
+        self.bloc_list = arcade.SpriteList()
 
         for i in range(125):
             bloc = arcade.Sprite('Image/textures/ground.png', SPRITE_SCALE)
@@ -77,14 +83,80 @@ class Cyberbot(arcade.Window):
             self.bloc_list.append(bloc)
 
         self.player = arcade.AnimatedWalkingSprite()
+        self.robot = arcade.AnimatedWalkingSprite()
         self.physics_engine = arcade.PhysicsEnginePlatformer(self.player,
                                                              self.bloc_list,
                                                              gravity_constant=GRAVITY)
+        
         # Gestion des sprite au repos (joueur)
         character_scale = 2
+        robot_scale = 0.25
         self.player.stand_right_textures = []
         self.player.stand_right_textures.append(arcade.load_texture("Image/Sprite/Dog/Chien_stand.png",
                                                                     scale=character_scale))
+
+        # Gestion des Sprites des robots
+        self.robot.walk_right_textures = []
+        self.robot.walk_right_textures.append(arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_000.png",
+                                                                  scale = robot_scale, mirrored= True))
+        self.robot.stand_right_textures = []
+
+        self.robot.stand_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_000.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_001.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_002.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_003.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_004.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_005.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_006.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_007.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_008.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_009.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_010.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_011.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_012.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_013.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_014.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_015.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_016.png",
+                                scale=robot_scale, mirrored= True))
+        self.robot.walk_right_textures.append(
+            arcade.load_texture("Image/Sprite/Robot_PNG/PNG_Animations/Robot1/06_Attack/Attack_017.png",
+                                scale=robot_scale, mirrored= True))
+        
+
         # Gestion des Sprites de mouvement du joueur
         self.player.walk_right_textures = []
         self.player.laser_strike_texture = []
@@ -109,12 +181,15 @@ class Cyberbot(arcade.Window):
         # Défini la vitesse d'afficahge des sprites joueurs
         self.player.texture_change_distance = 15
 
-        self.player.center_x = SCREEN_HEIGHT // 2
-        self.player.center_y = SCREEN_WIDTH // 12
+        self.player.center_x = SCREEN_WIDTH // 1.5
+        self.player.center_y = SCREEN_HEIGHT // 12
+        self.robot.center_x = SCREEN_WIDTH // 20
+        self.robot.center_y = SCREEN_HEIGHT// 4
 
         self.player.scale = 2
 
         self.all_sprites_list.append(self.player)
+        self.all_sprites_list.append(self.robot)
 
         # Routage de l'image background
         self.background = arcade.load_texture("Image/Background/cyberpunk-street-files/PNG/cyberpunk-street.png")
@@ -130,7 +205,6 @@ class Cyberbot(arcade.Window):
                                       SCREEN_WIDTH * 5 , SCREEN_HEIGHT, self.background, repeat_count_x=5)
         self.all_sprites_list.draw()
         self.bloc_list.draw()
-
 
     def update(self, delta_time):
         """ Movement and game logic """
@@ -148,6 +222,7 @@ class Cyberbot(arcade.Window):
                             SCREEN_HEIGHT + self.view_bottom)
 
         self.player.change_x = 2.5
+        self.robot.change_x = 5
 
     # Song activation
         if self.mute == 0:
@@ -159,15 +234,14 @@ class Cyberbot(arcade.Window):
         if key == arcade.key.SPACE:
             self.player.change_y = JUMP_SPEED
             self.count_y += self.player.change_y
-        if key == arcade.key.A:
-            self.
-
 
     def on_key_release(self, key, modifiers):
         """Called when the user releases a key. """
 
         if key == arcade.key.SPACE:
             self.player.change_y = -MOVEMENT_SPEED
+
+
 def main():
     window = Cyberbot()
     window.setup()
